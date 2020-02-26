7ec87c9a8d5291134386cdf52f0f723154f6cbc0
jimuel <jimuel.pron@unc.edu.ph>
2020-02-26 19:57:02 +0800

    giob

---
+++ app/Http/Controllers/InfosheetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\infosheet;
+use Illuminate\Http\Request;
+
+class InfosheetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\infosheet  $infosheet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(infosheet $infosheet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\infosheet  $infosheet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(infosheet $infosheet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\infosheet  $infosheet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, infosheet $infosheet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\infosheet  $infosheet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(infosheet $infosheet)
+    {
+        //
+    }
+}
---
+++ database/factories/InfosheetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\infosheet;
+use Faker\Generator as Faker;
+
+$factory->define(infosheet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
